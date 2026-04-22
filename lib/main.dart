800d903fceeb796f557a5e2cd82cde2dcf3087d7
David Mora Duque <david.mora02@uceva.edu.co>
2026-04-22 18:28:00 -0500

    Remover banner de debug de la app
    - Establecer debugShowCheckedModeBanner a false en MaterialApp.router

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Parcial Flutter',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
